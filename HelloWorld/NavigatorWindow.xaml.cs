78602d1f567c8ce29ede0661f1fd53e292bb5fe9
sabet <sabet@cstructor.com>
2017-03-13 20:56:24 -0700

    Session 5 - HelloWorld Resources

---
+++ HelloWorld/NavigatorWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics; // add this for the class exercise NavigatorWindow
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for NavigatorWindow.xaml
+    /// </summary>
+    public partial class NavigatorWindow : Window
+    {
+        public NavigatorWindow()
+        {
+            InitializeComponent();
+        }
+        // browse to the website based on given url
+        private void uxGo_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(uxUrl.Text));
+        }
+    }
+}
---
+++ HelloWorld/StatusWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for StatusWindow.xaml
+    /// </summary>
+    public partial class StatusWindow : Window
+    {
+        public StatusWindow()
+        {
+            InitializeComponent();
+        }
+        private void uxTextEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            int row = uxTextEditor.GetLineIndexFromCharacterIndex(uxTextEditor.CaretIndex);
+            int col = uxTextEditor.CaretIndex - uxTextEditor.GetCharacterIndexFromLineIndex(row);
+            uxStatus.Text = "Line " + (row + 1) + ", Char " + (col + 1);
+
+            // calculate the percentage complete: length / max length
+            int percent = 100 * uxTextEditor.Text.Length / uxTextEditor.MaxLength;
+
+            uxPercentComplete.Text = string.Format("{0}%", percent);
+
+            uxProgressBar.Value = percent; // Set the progressbar
+
+
+        }
+
+    }
+}
