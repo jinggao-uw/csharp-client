9beb0fef1daf1c1543d99c54159a6fc554de8ea5
sabet <sabet@cstructor.com>
2017-03-16 20:59:38 -0700

    session 6 - HelloWorld and Contact Application

---
+++ ContactApp/ContactApp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContactApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Do something when file is selected
+        private void uxFileNew_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new ContactWindow();
+
+            if (window.ShowDialog() == true)
+            {
+                App.ContactRepository.Add(window.Contact.ToRepositoryModel());
+            }
+        }
+    }
+}
---
+++ ContactApp/ContactRepository/DatabaseManager.cs
@@ -0,0 +1,25 @@
+﻿using ContactDB;
+
+namespace ContactRepository
+{
+    public class DatabaseManager
+    {
+        private static readonly ContactsEntities entities;
+
+        // Initialize and open the database connection
+        static DatabaseManager()
+        {
+            entities = new ContactsEntities();
+            entities.Database.Connection.Open();
+        }
+
+        // Provide an accessor to the database
+        public static ContactsEntities Instance
+        {
+            get
+            {
+                return entities;
+            }
+        }
+    }
+}
